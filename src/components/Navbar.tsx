3bc03fc48b4bc3458b824e135e34285566f64cea
new-user6666 <149027998+new-user6666@users.noreply.github.com>
2023-10-27 08:25:13 +0000

    to server

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,12 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Navbar() {
+  const pathname = usePathname();
+  useEffect(() => {
+    console.log(pathname);
+  }, [pathname]);
   return (
     <nav
       className="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start"
@@ -18,10 +26,10 @@ export default function Navbar() {
               className="text-sm pl-2 capitalize leading-normal text-slate-700 before:float-left before:pr-2 before:text-gray-600 before:content-['/']"
               aria-current="page"
             >
-              Dashboard
+              {pathname.slice(1)}
             </li>
           </ol>
-          <h6 className="mb-0 font-bold capitalize">Dashboard</h6>
+          <h6 className="mb-0 font-bold capitalize">{pathname.slice(1)}</h6>
         </nav>
         <div className="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
           <div className="flex items-center md:ml-auto md:pr-4">
@@ -37,10 +45,6 @@ export default function Navbar() {
             </div>
           </div>
           <ul className="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
-            {/* online builder btn  */}
-            {/* <li class="flex items-center">
-          <a class="inline-block px-8 py-2 mb-0 mr-4 text-xs font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro border-fuchsia-500 ease-soft-in hover:scale-102 active:shadow-soft-xs text-fuchsia-500 hover:border-fuchsia-500 active:bg-fuchsia-500 active:hover:text-fuchsia-500 hover:text-fuchsia-500 tracking-tight-soft hover:bg-transparent hover:opacity-75 hover:shadow-none active:text-white active:hover:bg-transparent" target="_blank" href="https://www.creative-tim.com/builder/soft-ui?ref=navbar-dashboard&amp;_ga=2.76518741.1192788655.1647724933-1242940210.1644448053">Online Builder</a>
-        </li> */}
             <li className="flex items-center">
               <a
                 href="./pages/sign-in.html"

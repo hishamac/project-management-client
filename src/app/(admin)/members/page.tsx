abb9a735f7c5075024e4c3481c1cec58e1fc4807
MUHAMMED HISHAM AC <122851090+hishamac@users.noreply.github.com>
2023-10-27 11:23:27 +0000

    connected to server

--- src/app/(admin)/members/page.tsx
+++ src/app/(admin)/members/page.tsx
@@ -1,10 +1,21 @@
+import { GetAllMemmbersDocument, GetAllMemmbersQuery, GetAllMemmbersQueryVariables } from "@/gql/graphql";
+import { getUrqlClient } from "@/lib/urql";
 import dynamic from "next/dynamic";
 
 const Members = dynamic(() => import("../../../components/Members"), {
   ssr: false,
 });
 
-const page = () => {
+const page = async () => {
+
+  const { client } = getUrqlClient();
+  const members = await client.query<
+    GetAllMemmbersQuery,
+    GetAllMemmbersQueryVariables
+  >(GetAllMemmbersDocument, {
+  });
+  
+
   return <Members />;
 };
 
